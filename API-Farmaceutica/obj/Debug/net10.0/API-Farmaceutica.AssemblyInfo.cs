95fc2906fb7b7ab47eb10509e9cbba3b8cafad8d
Carrion Tomas <carriontas@gmail.com>
2026-02-03 17:15:41 -0300

    addGitIgnore

--- API-Farmaceutica/obj/Debug/net10.0/API-Farmaceutica.AssemblyInfo.cs
+++ API-Farmaceutica/obj/Debug/net10.0/API-Farmaceutica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Farmaceutica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c6cc6ff33f4ee96b618064835708ef2ca71557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b78d4039770cc14f71972d7e646ea01cd55681")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Farmaceutica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Farmaceutica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
